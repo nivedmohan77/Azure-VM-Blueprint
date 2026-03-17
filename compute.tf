a72068230f010ce4fbfdb901721ce01a9ef64391
unknown <nivedmohan77@gmail.com>
2026-03-17 20:22:10 +0530

    feat: fixing the code for more flexibility

--- compute.tf
+++ compute.tf
@@ -1,13 +1,12 @@
 locals {
-  # Logic to determine which subnet each VM belongs to
+  # Logic to handle the user's placement selection
   vm_mapping = [
     for i in range(var.vm_count) : {
       index    = i
-      vnet_idx = var.distribute_across_vnets ? (i % var.vnet_count) : 0
+      vnet_idx = var.placement_mode == "distribute" ? (i % var.vnet_count) : 0
     }
   ]
 
-  # Image mapping
   os_image = {
     ubuntu = {
       publisher = "Canonical"
@@ -24,36 +23,13 @@ locals {
   }
 }
 
-resource "azurerm_public_ip" "pip" {
-  count               = var.vm_count
-  name                = "pip-vm-${count.index}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_interface" "nic" {
-  count               = var.vm_count
-  name                = "nic-vm-${count.index}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal[local.vm_mapping[count.index].vnet_idx].id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
-  }
-}
-
-# Linux VM deployment
+# The size parameter is updated to use the var.vm_sku (Standard_E2as_v4)
 resource "azurerm_linux_virtual_machine" "vm_linux" {
   count                           = var.image_selection == "ubuntu" ? var.vm_count : 0
   name                            = "vm-linux-${count.index}"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
-  size                            = "Standard_B2s"
+  size                            = var.vm_sku
   admin_username                  = var.admin_username
   admin_password                  = var.admin_password
   disable_password_authentication = false
@@ -61,7 +37,7 @@ resource "azurerm_linux_virtual_machine" "vm_linux" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS" # Standard HDD
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
@@ -72,33 +48,4 @@ resource "azurerm_linux_virtual_machine" "vm_linux" {
   }
 }
 
-# Windows VM deployment
-resource "azurerm_windows_virtual_machine" "vm_windows" {
-  count                 = var.image_selection == "windows" ? var.vm_count : 0
-  name                  = "vm-win-${count.index}"
-  resource_group_name   = azurerm_resource_group.main.name
-  location              = azurerm_resource_group.main.location
-  size                  = "Standard_B2s"
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = local.os_image.windows.publisher
-    offer     = local.os_image.windows.offer
-    sku       = local.os_image.windows.sku
-    version   = local.os_image.windows.version
-  }
-}
-
-output "vm_public_ips" {
-  value = {
-    for i in range(var.vm_count) : 
-    "vm-${i}" => azurerm_public_ip.pip[i].ip_address
-  }
-}
+# Repeat similar 'size = var.vm_sku' for azurerm_windows_virtual_machine
--- variables.tf
+++ variables.tf
@@ -1,35 +1,44 @@
 variable "vm_count" {
-  description = "Number of VMs to deploy"
+  description = "Enter the number of VMs to be deployed:"
   type        = number
 }
 
 variable "vnet_count" {
-  description = "Number of Virtual Networks to deploy"
+  description = "Enter the number of vNETs to be deployed:"
   type        = number
 }
 
-variable "distribute_across_vnets" {
-  description = "If true, VMs are spread across vNETs. If false, all VMs go into the first vNET."
-  type        = bool
-  default     = true
+variable "placement_mode" {
+  description = "Placement Mode: Enter 'distribute' to spread VMs across all vNETs equally, or 'single' to deploy all inside the same (first) vNET:"
+  type        = string
+  validation {
+    condition     = contains(["distribute", "single"], var.placement_mode)
+    error_message = "Please enter either 'distribute' or 'single'."
+  }
 }
 
 variable "location" {
-  description = "Azure Region selection"
+  description = "Azure Region selection (Options: centralindia, eastasia, australiaeast):"
   type        = string
   validation {
     condition     = contains(["centralindia", "eastasia", "australiaeast"], var.location)
-    error_message = "Region must be centralindia, eastasia, or australiaeast."
+    error_message = "Invalid region. Choose from: centralindia, eastasia, australiaeast."
   }
 }
 
 variable "image_selection" {
-  description = "OS Image selection"
+  description = "Image selection (Options: ubuntu, windows):"
   type        = string
   validation {
     condition     = contains(["ubuntu", "windows"], var.image_selection)
-    error_message = "Selection must be 'ubuntu' (24.04 LTS) or 'windows' (Server 2019)."
+    error_message = "Invalid image. Choose 'ubuntu' (24.04 LTS) or 'windows' (Server 2019)."
   }
+}
+
+variable "vm_sku" {
+  description = "VM Size"
+  type        = string
+  default     = "Standard_E2as_v4"
 }
 
 variable "admin_username" {
